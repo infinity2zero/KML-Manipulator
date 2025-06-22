333b2299eb37fdc4c19827f62310225e9f03ab41
Surya <suryasharmaiosdev@gmail.com>
2025-06-23 00:36:39 +0530

    ----

--- src/app/worker/intersects.worker.ts
+++ src/app/worker/intersects.worker.ts
@@ -1,7 +1,5 @@
 /// <reference lib="webworker" />
 import booleanIntersects from '@turf/boolean-intersects';
-
-
 addEventListener('message', ({ data }) => {
   if (data.type !== 'check') return;
   const hit = booleanIntersects(data.data.target, data.data.other);
